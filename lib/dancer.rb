05c79b65ac826ecb5bd160ecd5a2a992bd1bf970
Alex McKean <amckean@me.com>
2017-12-08 20:44:16 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,13 +2,5 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  #allow our classes to use all of the methods of the included module as *instance* methods.
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
 
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-end
+ends
